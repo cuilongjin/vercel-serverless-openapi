67973cadfd79613adb6b3f33e33e1edded625aaa
cuilongjin <cui3155@gmail.com>
2021-10-28 23:58:12 +0800

    火币接口

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 vercel dev
 vercel --prod
 ```
+
+接入URLs
+
+https://vercel-serverless-openapi.vercel.app
+
+火币接口
+
+https://huobiapi.github.io/docs/spot/v1/cn/#5ea2e0cde2
+
+示例
+
+https://huobiapi.github.io/docs/spot/v1/cn/#k
+
+https://api.huobi.pro/market/history/kline?symbol=btcusdt&period=1day&size=1
+
+https://vercel-serverless-openapi.vercel.app/api/huobi?api=market/history/kline&symbol=btcusdt&period=1day&size=1
--- api/huobi.js
+++ api/huobi.js
@@ -1,14 +1,18 @@
 /*
-test http://localhost:3000/api/huobi?api=open/api/v2/market/ticker&symbol=SHIB_USDT
+https://api.huobi.pro/market/detail/merged?symbol=btcusdt
+http://localhost:3000/api/huobi?api=market/detail/merged&symbol=btcusdt
 */
 
 import got from 'got'
+import qs from 'qs'
 module.exports = async (request, response) => {
   const { api, ...rest } = JSON.parse(JSON.stringify(request.query))
   const client = got.extend({
-    prefixUrl: 'https://www.mexc.com',
+    prefixUrl: 'https://api.huobi.pro/',
     responseType: 'json'
   })
-  const result = await client(api, rest)
+
+  const query = qs.stringify(rest)
+  const result = await client(`${api}?${query}`)
   response.status(200).json(result.body)
 }
