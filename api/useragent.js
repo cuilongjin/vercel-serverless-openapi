27305800b7da3a89c593aac34c43169a355456f6
cuilongjin <cui3155@gmail.com>
2022-01-02 13:05:52 +0800

    更新

---
+++ api/useragent.js
@@ -0,0 +1,14 @@
+// chrome Win
+export const chromeWin = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+
+// 今日头条 ios客户端
+export const toutiaoIOS = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NewsArticle/8.1.5.12 JsSdk/2.0 NetType/WIFI (News 8.1.5 14.100000)'
+
+// bilibili ios客户端
+export const bilibiliIOS = 'bili-universal/64700100 CFNetwork/1197 Darwin/20.0.0 os/ios model/iPhone 8 Plus mobi_app/iphone build/64700100 osVer/14.1 network/2 channel/AppStore'
+
+// 腾讯视频 ios客户端
+export const qqVideoIOS = 'live4iphoneRel/25394 CFNetwork/1197 Darwin/20.0.0'
+
+// safari
+export const safari = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
