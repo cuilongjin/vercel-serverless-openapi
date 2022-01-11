e5a843b0f374f9b52834a943f1784440da284ffd
cuilongjin <cui3155@gmail.com>
2022-01-11 17:25:01 +0800

    更新

--- utils/index.js
+++ utils/index.js
@@ -3,14 +3,16 @@
   *  @param {number} len 长度
   *  @return {string} 随机字符串
 */
-
-export function randomWord (len) {
-  let str = ''
-  const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
+function randomWord (len) {
+  let o = ''
+  const s = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   for (let i = 0; i < len; i++) {
-    const pos = Math.round(Math.random() * (arr.length - 1))
-    str += arr[pos]
+    const pos = Math.round(Math.random() * (s.length - 1))
+    o += s[pos]
   }
-  return str
+  return o
+}
+
+module.exports = {
+  randomWord
 }
